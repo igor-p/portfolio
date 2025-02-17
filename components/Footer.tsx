61bc856feb97a90dc750c7c993cb7e358c7142d3
Igor Pokryshevskiy <ipokrysh@gmail.com>
2025-02-16 23:02:22 -0500

    update footer content and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,20 +5,16 @@ import { IconLink } from "./IconLink"
 
 export function Footer() {
   return (
-    <footer className="border-t">
-      <div className="container flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
-        <p className="text-xs text-gray-500 dark:text-gray-400">
-          © 2025 Igor.nyc. All rights reserved.
-        </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <IconLink
-            name="GitHub"
-            url={URL_GITHUB}
-            Icon={IconGithub}
-            variant="ghost"
-          />
-        </nav>
-      </div>
+    <footer className="border-t flex justify-between gap-2 w-full shrink-0 items-center px-4 py-6 md:px-6">
+      <p className="text-xs text-gray-500 dark:text-gray-400">
+        © 2025 Igor Pokryshevskiy. All rights reserved.
+      </p>
+      <IconLink
+        name="GitHub"
+        url={URL_GITHUB}
+        Icon={IconGithub}
+        variant="ghost"
+      />
     </footer>
   )
 }
